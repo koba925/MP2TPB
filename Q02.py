8281083ec9cf682250cc6eebac1258a5a2c1a346
Kobayashi Takahiro <koba925@gmail.com>
2023-05-01 22:29:29 +0900

    Q02 Refactor

--- Q02.py
+++ Q02.py
@@ -1,8 +1,5 @@
 # Q02.py
 
-def reverse(s):
-    return "".join(list(reversed(s)))
-
 # ops = ["+", "-", "*", "//", ""]
 ops = ["*", ""]
 
@@ -11,12 +8,12 @@ for n in range(1000, 10000):
     for op1 in ops:
         for op2 in ops:
             for op3 in ops:
-                expr = nstr[0] + op1 + nstr[1] + op2 + nstr[2] + op3 + nstr[3]
+                expr = nstr[3] + op3 + nstr[2] + op2 + nstr[1] + op1 + nstr[0]
                 if expr == nstr:
                     continue
                 try:
                     val = eval(expr)
                 except:
                     continue
-                if str(n) == reverse(str(val)):
+                if n == val:
                     print(f"{n} = {expr}")
